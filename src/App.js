622b79a0f4bdf0e22cc7f1754e3403193bc00e78
AndrzejPec <pimatstudio@gmail.com>
2023-12-12 22:05:47 +0100

    inserted first components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
+import { Header } from './components/Header';
+import { Image } from './components/Image';
+import { Hero } from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+        <Header />
+        <Image align={"right"}/>
+        <Hero />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+    // component code here
+  }
